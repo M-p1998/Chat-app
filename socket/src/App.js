8e15b7344ce1f5dfd84f49e7b46e7c383bb1229e
Mya <skyusmpp@gmail.com>
2023-08-07 10:20:08 -0700

    test

---
+++ socket/src/App.js
@@ -0,0 +1,37 @@
+
+import {useEffect, useState}  from "react"
+import './App.css';
+import io from 'socket.io-client';
+import Form from "./components/Form";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Homepage from "./components/Homepage";
+
+function App() {
+
+  const [socket] = useState(() =>io(':8000'));
+  const [username, setUsername] = useState('')
+  const [isConnected, setIsConnected] = useState(socket.connected);
+
+  useEffect(() => {
+    socket.on('connect', () =>{
+      setIsConnected(true)
+    })
+    return () => {
+      socket.disconnect(true)
+    }
+  },[])
+  return (
+
+    <div className="App">
+      <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Form socket={socket} username={username} setUsername={setUsername} />}/>
+            <Route path="/homepage" element={<Homepage socket={socket} username={username} setUsername={setUsername}/>}/>
+       
+          </Routes>  
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
