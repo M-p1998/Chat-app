9b2f6503df4653467b96ee8c018868903b4ac119
Mya <skyusmpp@gmail.com>
2023-08-08 09:12:32 -0700

    show only one uses's messages on both page

--- socket/src/components/Homepage.js
+++ socket/src/components/Homepage.js
@@ -1,91 +1,9 @@
 
-// import React from 'react';
-// import {useState, useEffect} from "react";
-
-// const Homepage = (props) => {
-//     const {socket,  username, setUsername} = props
-//     const [users, setUsers] = useState([])
-//     const [input, setInput] = useState('')
-//     // const [messages, setMessages] = useState({})
-
-//     const [userMessages, setUserMessages] = useState({});
-
-//     useEffect(() =>{
-
-//         socket.on(`new-user-joined`, data => {
-//             console.log(data);
-//             setUsers(data)
-//         });
-//         socket.on('send-message-to-all-clients', (data) =>{
-//             // setMessages([...messages, data])
-//             setUserMessages((prevUserMessages) => {
-//                 const userId = data.userId;
-//                 const updatedMessages = [...(prevUserMessages[userId] || []), data];
-//                 return { ...prevUserMessages, [userId]: updatedMessages };
-//               });
-//         })
-//     }, [])
-
-//     const sendMessage = (e) =>{
-//         e.preventDefault();
-//         socket.emit('send-message', {message: input, username: username})
-//     }
-
-//     return (
-//         <div>
-//           <h2>Users in the chat</h2>
-//           {users.map((user) => (
-//             <p key={user.id}>username: {user.username}</p>
-//           ))}
-//           <form onSubmit={sendMessage}>
-//             <label>Message</label>
-//             <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
-//             <button>Send Message</button>
-//           </form>
-//           {users.map((user) => (
-//             <div key={user.id}>
-//               <h3>Conversation with {user.username}</h3>
-//               {userMessages[user.id] &&
-//                 userMessages[user.id].map((message, index) => (
-//                   <div key={index}>
-//                     {message.username} Says: {message.message}
-//                   </div>
-//                 ))}
-//             </div>
-//           ))}
-//         </div>
-//       );
-
-// //   return (
-// //     <div>
-// //         <h2>Users in the chat</h2>
-// //         {
-// //             users.map((user) => (
-// //                 <p key={user.id}>username: {user.username}</p>
-// //             ))
-// //         }
-// //         <form onSubmit={sendMessage}>
-// //             <label>Message</label>
-// //             <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
-// //             <button>Send Message</button>
-// //         </form>
-// //         {
-// //             messages.map((message, index) =>(
-// //                 <div key={index} >{message.username} Says: {message.message} </div>
-// //             ))
-// //         }
-// //     </div>
-// //   )
-// }
-
-// export default Homepage
-
-
 
 import React, { useState, useEffect } from 'react';
 
 const Homepage = (props) => {
-  const { socket, username, setUsername } = props;
+  const { socket, username } = props;
   const [users, setUsers] = useState([]);
   const [input, setInput] = useState('');
   const [userMessages, setUserMessages] = useState({});
@@ -112,83 +30,80 @@ const Homepage = (props) => {
     e.preventDefault();
     socket.emit('send-message', { message: input, username: username });
     setInput('');
+    // Update userMessages state with the new message
+    setUserMessages((prevUserMessages) => {
+      const updatedMessages = [...(prevUserMessages[socket.id] || []), { message: input, username: username }];
+      return { ...prevUserMessages, [socket.id]: updatedMessages };
+    });
   };
 
-
-  // return (
-  //   <div>
-  //     <h2>Users in the chat</h2>
-  //     {users.map((user) => (
-  //       <p key={user.id}>username: {user.username}</p>
-  //     ))}
-  //     {chatFullMessage && <p>{chatFullMessage}</p>}
-  //     {users.length === 2 && !chatFullMessage && (
-  //       <form onSubmit={sendMessage}>
-  //         <label>Message</label>
-  //         <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
-  //         <button>Send Message</button>
-  //       </form>
-  //     )}
-  //     {users.map((user) => (
-  //       <div key={user.id}>
-  //         <h3>Conversation with {user.username}</h3>
-  //         {userMessages[user.id] &&
-  //           userMessages[user.id].map((message, index) => (
-  //             <div key={index}>
-  //               {message.username} Says: {message.message}
-  //             </div>
-  //           ))}
-  //       </div>
-  //     ))}
-
-
-  //   </div>
-  // );
-
   
-
   return (
     <div>
       <h2>Users in the chat</h2>
       {users.map((user) => (
         <p key={user.id}>username: {user.username}</p>
       ))}
-      {chatFullMessage && <p>{chatFullMessage}</p>}
-      {/* {users.length === 2 && !chatFullMessage && ( */}
-        <form onSubmit={sendMessage}>
-          <label>Message</label>
-          <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
-          <button>Send Message</button>
-        </form>
-      {/* )} */}
-      {users.length === 2 && !chatFullMessage && (
+      {/* {chatFullMessage && <p>{chatFullMessage}</p>} */}
+      {/* {users.length === 2 && !chatFullMessage && (
         <div>
           <h3>Conversation between {users[0].username} and {users[1].username}</h3>
           <div className="conversation">
-            {userMessages[users[0].id] && userMessages[users[1].id] && (
-              userMessages[users[0].id].map((message, index) => (
-                <div  className={`message ${message.userId === users[0].id ? 'self' : 'other'}`} key={index}>
-                  {message.username} says: {message.message}
-                </div>
-              )).concat(
-                userMessages[users[1].id]&&
-                userMessages[users[0].id] &&
-                userMessages[users[1].id].map((message, index) => (
-                  <div className={`message ${message.userId === users[1].id ? 'self' : 'other'}`} key={index}>
+            {(userMessages[users[0].id] || userMessages[users[1].id]) && (
+              <>
+                {userMessages[users[0].id]?.map((message, index) => (
+                  <div
+                    className={`message ${message.username === username ? 'self' : 'other'}`}
+                    key={index}
+                  >
                     {message.username} says: {message.message}
                   </div>
-                ))
-              )
+                ))}
+                {userMessages[users[1].id]?.map((message, index) => (
+                  <div
+                    className={`message ${message.username === username ? 'self' : 'other'}`}
+                    key={index}
+                  >
+                    {message.username} says: {message.message}
+                  </div>
+                ))}
+              </>
             )}
           </div>
+          <form onSubmit={sendMessage}>
+            <label>Message</label>
+            <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+            <button>Send Message</button>
+          </form>
+        </div>
+      )} */}
+      {users.length === 2 && !chatFullMessage && (
+  <div>
+    <h3>Conversation between {users[0].username} and {users[1].username}</h3>
+    <div className="conversation">
+      {users.map(user => (
+        userMessages[user.id]?.map((message, index) => (
+          <div
+            className={`message ${message.username === username ? 'self' : 'other'}`}
+            key={index}
+          >
+            {message.username} says: {message.message}
+            
           </div>
-      )}
+        ))
+      ))}
+    </div>
+    <form onSubmit={sendMessage}>
+      <label>Message</label>
+      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+      <button>Send Message</button>
+    </form>
+  </div>
+)}
     </div>
   );
-
-
-
-
 };
 
 export default Homepage;
+
+
